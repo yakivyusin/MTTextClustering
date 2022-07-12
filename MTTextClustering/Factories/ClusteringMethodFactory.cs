12f12318ae13cf2a120750ad61e4403b8470d725
Yakiv Yusin <yusin.yakiv@gmail.com>
2022-07-12 16:00:40 +0300

    Add Method Factories

---
+++ MTTextClustering/Factories/ClusteringMethodFactory.cs
@@ -0,0 +1,14 @@
+﻿using MTTextClustering.Methods.Contracts;
+using MTTextClustering.Models;
+using System;
+
+namespace MTTextClustering.Factories
+{
+    public static class ClusteringMethodFactory
+    {
+        public static IClusteringMethod Create(ClusteringMethods method) => method switch
+        {
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
---
+++ MTTextClustering/Factories/KClusteringMethodFactory.cs
@@ -0,0 +1,14 @@
+﻿using MTTextClustering.Methods.Contracts;
+using MTTextClustering.Models;
+using System;
+
+namespace MTTextClustering.Factories
+{
+    public static class KClusteringMethodFactory
+    {
+        public static IKClusteringMethod Create(ClusteringMethods method) => method switch
+        {
+            _ => throw new NotImplementedException(),
+        };
+    }
+}
