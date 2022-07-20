d9e11f0f6bdfc1207557892452b13a5fd21a2a15
Yakiv Yusin <yusin.yakiv@gmail.com>
2022-07-20 08:37:30 +0300

    Small Refactoring

--- MTTextClustering.Methods/Evaluation/DunnIndex.cs
+++ MTTextClustering.Methods/Evaluation/DunnIndex.cs
@@ -1,12 +1,11 @@
-﻿using MTTextClustering.Methods.Contracts;
-using MTTextClustering.Methods.Helpers;
+﻿using MTTextClustering.Methods.Helpers;
 using MTTextClustering.Models;
 
 namespace MTTextClustering.Methods.Evaluation
 {
-    public class DunnIndex : IEvaluationMethod
+    public class DunnIndex : MetricEvaluationBase
     {
-        public double Evaluate(Text[] texts, List<List<Guid>> clusters)
+        public override double Evaluate(Text[] texts, List<List<Guid>> clusters)
         {
             var tfIdf = new TfIdfCalculator(texts).GetTfIdfMatrix();
 
@@ -24,71 +23,6 @@ namespace MTTextClustering.Methods.Evaluation
             }
 
             return minimumInterDistance / maximumIntraDistance;
-        }
-
-        private double GetIntraDistance(Text[] texts, List<Guid> cluster, double[][] tfIdf)
-        {
-            var clusterTexts = texts.Where(x => cluster.Contains(x.Id)).ToArray();
-            var clusterVectors = clusterTexts
-                .Select(x => Array.IndexOf(texts, x))
-                .Select(x => tfIdf[x])
-                .ToArray();
-
-            var maximumPairDistance = Double.MinValue;
-
-            for (int i = 0; i < clusterVectors.Length; i++)
-            {
-                for (int j = i + 1; j < clusterVectors.Length; j++)
-                {
-                    var distance = 0.0;
-
-                    for (int k = 0; k < clusterVectors[i].Length; k++)
-                    {
-                        distance += Math.Pow(clusterVectors[i][k] - clusterVectors[j][k], 2);
-                    }
-
-                    distance = Math.Sqrt(distance);
-                    maximumPairDistance = Math.Max(maximumPairDistance, distance);
-                }
-            }
-
-            return maximumPairDistance;
-        }
-
-        private double GetInterDistance(Text[] texts, List<Guid> cluster1, List<Guid> cluster2, double[][] tfIdf)
-        {
-            var cluster1Vectors = texts
-                .Where(x => cluster1.Contains(x.Id))
-                .Select(x => Array.IndexOf(texts, x))
-                .Select(x => tfIdf[x])
-                .ToArray();
-
-            var cluster2Vectors = texts
-                .Where(x => cluster2.Contains(x.Id))
-                .Select(x => Array.IndexOf(texts, x))
-                .Select(x => tfIdf[x])
-                .ToArray();
-
-            var minDistance = double.MaxValue;
-
-            foreach (var vector in cluster1Vectors)
-            {
-                foreach (var otherVector in cluster2Vectors)
-                {
-                    var distance = 0.0;
-
-                    for (int i = 0; i < vector.Length; i++)
-                    {
-                        distance += Math.Pow(vector[i] - otherVector[i], 2);
-                    }
-
-                    distance = Math.Sqrt(distance);
-
-                    minDistance = Math.Min(minDistance, distance);
-                }
-            }
-
-            return minDistance;
         }
     }
 }
---
+++ MTTextClustering.Methods/Evaluation/MetricEvaluationBase.cs
@@ -0,0 +1,74 @@
+﻿using MTTextClustering.Methods.Contracts;
+using MTTextClustering.Models;
+
+namespace MTTextClustering.Methods.Evaluation
+{
+    public abstract class MetricEvaluationBase : IEvaluationMethod
+    {
+        public abstract double Evaluate(Text[] texts, List<List<Guid>> clusters);
+
+        protected double GetIntraDistance(Text[] texts, List<Guid> cluster, double[][] tfIdf)
+        {
+            var clusterVectors = texts
+                .Where(x => cluster.Contains(x.Id))
+                .Select(x => Array.IndexOf(texts, x))
+                .Select(x => tfIdf[x])
+                .ToArray();
+
+            var maximumPairDistance = Double.MinValue;
+
+            for (int i = 0; i < clusterVectors.Length; i++)
+            {
+                for (int j = i + 1; j < clusterVectors.Length; j++)
+                {
+                    var distance = GetVectorDistance(clusterVectors[i], clusterVectors[j]);
+
+                    maximumPairDistance = Math.Max(maximumPairDistance, distance);
+                }
+            }
+
+            return maximumPairDistance;
+        }
+
+        protected double GetInterDistance(Text[] texts, List<Guid> cluster1, List<Guid> cluster2, double[][] tfIdf)
+        {
+            var cluster1Vectors = texts
+                .Where(x => cluster1.Contains(x.Id))
+                .Select(x => Array.IndexOf(texts, x))
+                .Select(x => tfIdf[x])
+                .ToArray();
+
+            var cluster2Vectors = texts
+                .Where(x => cluster2.Contains(x.Id))
+                .Select(x => Array.IndexOf(texts, x))
+                .Select(x => tfIdf[x])
+                .ToArray();
+
+            var minDistance = double.MaxValue;
+
+            foreach (var vector in cluster1Vectors)
+            {
+                foreach (var otherVector in cluster2Vectors)
+                {
+                    var distance = GetVectorDistance(vector, otherVector);
+
+                    minDistance = Math.Min(minDistance, distance);
+                }
+            }
+
+            return minDistance;
+        }
+
+        private double GetVectorDistance(double[] vector1, double[] vector2)
+        {
+            var distance = 0.0;
+
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                distance += Math.Pow(vector1[i] - vector2[i], 2);
+            }
+
+            return Math.Sqrt(distance);
+        }
+    }
+}
