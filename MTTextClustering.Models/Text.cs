113cedddfd2302140fda6b89ba98249bb53e0117
Yakiv Yusin <yusin.yakiv@gmail.com>
2022-07-20 09:42:49 +0300

    Change Text Model

--- MTTextClustering.Models/Text.cs
+++ MTTextClustering.Models/Text.cs
@@ -2,7 +2,12 @@
 {
     public record Text(
         Guid Id,
-        string[] Terms)
+        string Content)
     {
+        private readonly Lazy<string[]> _terms = new(() => Content.Split(
+            new[] { "\r\n", "\r", "\n", " " },
+            StringSplitOptions.RemoveEmptyEntries));
+
+        public string[] Terms => _terms.Value;
     }
 }
